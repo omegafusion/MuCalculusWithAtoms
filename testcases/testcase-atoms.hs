f8b6137ec3bc402df37a6a4ceadc833f55212c5a
omegafusion <39763713+omegafusion@users.noreply.github.com>
2022-12-19 18:32:56 +0000

    Fixed testcase

--- testcases/testcase-atoms.hs
+++ testcases/testcase-atoms.hs
@@ -24,12 +24,12 @@ main = let states :: NL.Set State
            transRel :: TransRel
            transRel = NL.unions [NL.map (\a -> (State 0 [], State 0 [a])) NL.atoms, NL.map (\a -> (State 0 [a], State 0 [a])) NL.atoms]
            satRel :: SatRel
-           satRel = NL.map (\a -> (State 0 [a], Pred a)) NL.atoms
+           satRel = NL.map (\a -> (State 0 [a], Pred 0 [a])) NL.atoms
            model :: KripkeModel
            model = (states, transRel, satRel)
 
            vx = Var 0 []
-           pa = Pred (NL.constant 5)
+           pa = Pred 0 [NL.constant 5]
            formula1 = Mu vx (Disjunction (Predicate pa) (Diamond (Variable vx))) 
        in do
            P.print $ check model formula1   -- {State 0 [], State 0 [5]}
