aa24fabc685b6f174ea4338fac61d6ba2c14d6f5
omegafusion <39763713+omegafusion@users.noreply.github.com>
2023-01-03 20:04:07 +0000

    Made a CTLModelChecker module

--- CTLModelChecker.hs
+++ CTLModelChecker.hs
@@ -1,3 +1,8 @@
+module CTLModelChecker (
+    State (..),
+    check
+) where
+
 import Prelude hiding (True, False, filter, map, not, or, sum, toList)
 import qualified Prelude as P
 
@@ -70,14 +75,3 @@ check model formula =
                 let s = check' p 
                 in checkExistsGlobally s
     in check' formula
-
-
-main :: P.IO()
-main = let states = Set.fromList [State 0, State 1, State 2]
-           transRel = Set.fromList [(State 0, State 1), (State 1, State 2)]
-           satRel = Set.fromList [(State 0, Pred 0), (State 1, Pred 1), (State 2, Pred 2), (State 0, Pred 3), (State 1, Pred 3), (State 2, Pred 4)]
-           ks = (states, transRel, satRel)
-           f = ExistsUntil (Predicate (Pred 0)) (Predicate (Pred 2))
-           g = ExistsUntil (Predicate (Pred 3)) (Predicate (Pred 4))
-       in do print $ check ks f;
-             print $ check ks g;
