f04a9106464d548d29a1b1fb2d413dc4dec1e605
omegafusion <39763713+omegafusion@users.noreply.github.com>
2023-01-05 02:22:22 +0000

    Added vector mu calculus to syntax

--- MuSyntax.hs
+++ MuSyntax.hs
@@ -6,6 +6,7 @@ module MuSyntax
 ) where
 
 import Data.List ( elemIndex )
+import Data.Maybe ( fromJust )
 
 newtype Pred = Pred Int deriving (Show, Eq, Ord)
 
@@ -47,10 +48,12 @@ instance Eq Formula where
         p == p'
     Diamond p == Diamond p' =
         p == p'
-    Mu _ [(x, p)] == Mu _ [(x', p')] =
-        let fv = freeVars p ++ freeVars p'
-            y = freshFrom fv
-        in nameswap x y p == nameswap x' y p'
+    Mu z ps == Mu z' ps' =
+        let checkRow (x, p) (x', p') =
+                let fv = freeVars p ++ freeVars p'
+                    y = freshFrom fv
+                in nameswap x y p == nameswap x' y p'
+        in length ps == length ps' && and (zipWith checkRow ps ps') && findIndex z ps == findIndex z' ps'
 
 
 {-dual :: Formula -> Formula
@@ -67,7 +70,7 @@ freeVars formula =
                     Disjunction p q -> fvs xs p ++ fvs xs q
                     Negation p -> fvs xs p
                     Diamond p -> fvs xs p
-                    Mu _ [(x, p)] -> fvs (x:xs) p
+                    Mu _ ps -> concatMap (\(x, p) -> fvs (x:xs) p) ps
     in fvs [] formula
 
 
@@ -88,7 +91,9 @@ nameswap x y formula =
             Disjunction p q -> Disjunction (nameswap x y p) (nameswap x y q)
             Negation p -> Negation (nameswap x y p)
             Diamond p -> Diamond (nameswap x y p)
-            Mu _ [(z, p)] -> let z' = ns x y z in Mu z' [(z', nameswap x y p)]
+            Mu z ps -> let z' = ns x y z
+                           nameswapRow (w, p) = (ns x y w, nameswap x y p)
+                       in Mu z' (map nameswapRow ps)
 
 
 substitute :: Var -> Formula -> Formula -> Formula
@@ -102,10 +107,12 @@ substitute x t =
         sub (Negation p) = Negation (sub p)
         sub (Disjunction p q) = Disjunction (sub p) (sub q)
         sub (Diamond p) = Diamond (sub p)
-        sub (Mu _ [(y, p)])
-            | x==y         = Mu y [(y, p)] -- x does not occur free in p
-            | y `elem` fv  = let z = freshFrom fv in Mu z [(z, sub (substitute y (Variable z) t))]
-            | otherwise    = Mu y [(y, sub p)]
-            -- if the variable we're substituting is bound,
-            -- it's not really the same variable  
+        sub (Mu x ps) =
+            let subRow (y, p)
+                    | x == y       = (y, p)
+                    | y `elem` fv  = let z = freshFrom fv in (z, sub (substitute y (Variable z) t))
+                    | otherwise    = (y, sub p)
+                ps' = map subRow ps
+                x' = fst (ps' !! fromJust (findIndex x ps))
+            in Mu x' ps'
     in sub
